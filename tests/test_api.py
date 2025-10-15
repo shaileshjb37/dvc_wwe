0a355e400f4ce1f486bd789c58335fd49a397a83
shaileshjb37 <shaileshjb37@gmail.com>
2025-10-15 11:10:50 +0530

    New Commit

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,7 @@
 # tests/test_api.py
 from fastapi.testclient import TestClient
-import sys, os
+import sys
+import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app import app
 
